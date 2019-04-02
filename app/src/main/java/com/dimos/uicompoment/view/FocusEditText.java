e833915f8c87f094d2cdbee57d9c9fb374a4ef84
zhangyu <zhangyu@sunmi.com>
2019-04-02 11:07:06 +0800

    添加注释

--- app/src/main/java/com/dimos/uicompoment/view/FocusEditText.java
+++ app/src/main/java/com/dimos/uicompoment/view/FocusEditText.java
@@ -3,8 +3,6 @@ package com.dimos.uicompoment.view;
 import android.content.Context;
 import android.graphics.Rect;
 import android.util.AttributeSet;
-import android.view.MotionEvent;
-import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
